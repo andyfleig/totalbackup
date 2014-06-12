90de6e9932c9ebbbaccae31711b40e01d774062c
andy <andy.fleig@gmail.com>
2014-06-12 16:32:20 +0200

    + JavaDoc in Controller

--- src/src/main/Controller.java
+++ src/src/main/Controller.java
@@ -9,11 +9,14 @@ import java.util.ArrayList;
 
 public class Controller {
 
-	Mainframe mainframe;
+	private Mainframe mainframe;
 	private int numberOfSources = 0;
 	private ProgramState programState = ProgramState.getInstance(this);
 	private static final String PROPERTIES_PATH = "./properties.properties";
 
+	/**
+	 * Startet und initialisiert den Controller.
+	 */
 	public void startController() {
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -24,14 +27,25 @@ public class Controller {
 		});
 	}
 
+	/**
+	 * Läd die Programmeinstellungen aus der Properties-Datei und wendet sie an.
+	 */
 	public void loadProps() {
 		programState.loadProperties(PROPERTIES_PATH);
 	}
 
+	/**
+	 * Speichert die Programmeinstellungen in die Properties-Datei.
+	 */
 	public void saveProbs() {
 		programState.saveProperties(PROPERTIES_PATH);
 	}
 
+	/**
+	 * Startet ein "normales" Backup.
+	 * @param source Quellpfad
+	 * @param destination Zielpfad
+	 */
 	public void startBackup(String source, String destination) {
 		Backup backup = new Backup(this, source, destination);
 		try {
@@ -43,29 +57,58 @@ public class Controller {
 		}
 	}
 
+	/**
+	 * Gibt den übergebenen String auf dem Output-Panel aus.
+	 * @param s auszugebender String
+	 */
 	public void printOut(String s) {
 		mainframe.addToOutput(s);
 	}
 	
+	/**
+	 * Gibt den Zielpfad als String zurück.
+	 * @return Zielpfad
+	 */
 	public String getDestinationPath() {
 		return mainframe.getDestPath();
 	}
 	
+	/**
+	 * Setzt den Zielpfad auf den übergebenen String.
+	 * @param path zu setzender Zielpfad
+	 */
 	public void setDestinationPath(String path) {
 		mainframe.setDestPath(path);
 	}
+	
+	/**
+	 * Gibt Anzahl der eingetragenen Quellverzeichnisse zurück.
+	 * @return Anzahl der Quellverzeichnisse.
+	 */
 	public int getNumberOfSources() {
 		return numberOfSources;
 	}
 	
+	/**
+	 * Legt die Anzahl der Quellverzeichnisse auf den übergebenen Wert.
+	 * @param number zu setzende Anzahl der Quellverzeichnisse
+	 */
 	public void setNumberOfSources(int number) {
 		this.numberOfSources = number;
 	}
 	
+	/**
+	 * Gibt alle Quellpfade als ArrayList zurück.
+	 * @return Quellpfade
+	 */
 	public ArrayList<String> getSourcePaths() {
 		return mainframe.getSourcePaths();
 	}
 	
+	/**
+	 * Legt die Quellpfade auf die übergebenen Pfade fest. Alte Quellpfade werden dabei überschrieben.
+	 * @param sourcePaths zu setzende Quellpfade
+	 */
 	public void setSourcePaths(ArrayList<String> sourcePaths) {
 		mainframe.setSourcePaths(sourcePaths);
 		numberOfSources = sourcePaths.size();
