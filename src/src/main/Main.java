be584a3a9a235f15e80f17ab9c74243d0470fe21
andy <andy.fleig@gmail.com>
2014-06-12 16:26:45 +0200

    + JavaDoc in Main

--- src/src/main/Main.java
+++ src/src/main/Main.java
@@ -4,6 +4,10 @@ import main.Controller;
 
 public class Main {
 
+	/**
+	 * Main-Methode zum starten des Programms
+	 * @param args Start-Argumente
+	 */
 	public static void main(String[] args) {
 
 		Controller c = new Controller();
