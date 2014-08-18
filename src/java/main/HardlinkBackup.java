75b4debd26d72bcab282bcfceb6cbfe3ddeccbad
andy <andy.fleig@gmail.com>
2014-08-18 16:49:42 +0200

    * Datenstruktur wird kann nun erzeugt werden

--- src/java/main/HardlinkBackup.java
+++ src/java/main/HardlinkBackup.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.File;
 import java.util.ArrayList;
 
 public class HardlinkBackup implements Backupable {
@@ -9,6 +10,7 @@ public class HardlinkBackup implements Backupable {
 	private ArrayList<String> sourcePaths;
 	private String destinationPath;
 	private Controller controller;
+	private ArrayList<StructureFile> directoryStructure;
 	
 	
 	/**
@@ -21,13 +23,52 @@ public class HardlinkBackup implements Backupable {
 		this.controller = c;
 		this.sourcePaths = sources;
 		this.destinationPath = destination;
+		directoryStructure = new ArrayList<StructureFile>();
 	}
 
 
 	@Override
 	public void runBackup(String taskName) throws FileNotFoundException, IOException {
-		// TODO implementation
 		
+		// Kontrollieren ob die Verzeichnisstruktur vorhanden ist:
+		// TODO
+		// Wenn nein, Verzeichnisstruktur anlegen/ berechnen:
+		//TODO
+		
+		// Eigentliches Hardlink-Backup
+		
+	}
+	
+	/**
+	 * Erzeugt die Verzeichnisstruktur.
+	 */
+	private void createDirectoryStructure() {
+		
+		// Verzeichnisstruktur-Objekt erzeugen:
+		for (int i = 0; i < sourcePaths.size(); i++) {
+			StructureFile rootFile = recCalcDirStruct(sourcePaths.get(i), sourcePaths.get(i));
+			directoryStructure.add(rootFile);
+		}
+		// Verzeichnisstruktur speichern:
+		//TODO
+		
+	}
+	
+	private StructureFile recCalcDirStruct(String rootPath, String path) {
+		File currentFile = new File(path);
+		File[] files = currentFile.listFiles();
+		
+		StructureFile sFile = new StructureFile(rootPath, path);
+		for (int i = 0; i < files.length; i++) {
+			StructureFile newFile;
+			if (files[i].isDirectory()) {
+				newFile = recCalcDirStruct(rootPath, files[i].getAbsolutePath());
+			} else {
+				newFile = new StructureFile(rootPath, files[i].getAbsolutePath());
+			}
+			sFile.addFile(newFile);
+		}
+		return sFile;
 	}
 
 }
