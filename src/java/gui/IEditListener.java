a66376fc2d4b5a55a8c2717bf36e8106e8b3b950
andy <andy.fleig@gmail.com>
2014-10-01 11:26:37 +0200

    + JavaDoc für IEditListener

--- src/java/gui/IEditListener.java
+++ src/java/gui/IEditListener.java
@@ -5,11 +5,36 @@ import java.util.ArrayList;
 import main.BackupTask;
 
 public interface IEditListener {
+	/**
+	 * Liefert den Backup-Task mit gegebenem Namen zurück. Exisitert kein Backup
+	 * mit dem angegebenen Namen so wird null zurückgeliefert.
+	 * 
+	 * @param name
+	 *            Name des "gesuchten" Backup-Tasks
+	 * @return den gesuchten Backup-Task oder null
+	 */
 	public BackupTask getBackupTaskWithName(String name);
-	
+
+	/**
+	 * Löscht einen Backup-Task.
+	 * 
+	 * @param task
+	 *            zu löschender Backup-Task.
+	 */
 	public void removeBackupTask(BackupTask task);
-	
+
+	/**
+	 * Gibt eine Liste mit allen Namen aller Backup-Tasks zurück.
+	 * 
+	 * @return Liste der Namen aller Backup-Tasks
+	 */
 	public ArrayList<String> getBackupTaskNames();
-	
+
+	/**
+	 * Fügt einen Backup-Task hinzu.
+	 * 
+	 * @param task
+	 *            hinzuzufügender Backup-Task
+	 */
 	public void addBackupTask(BackupTask task);
 }
