2c800641f20b74119e77e35af426bb17d7a7049f
andy <andy.fleig@gmail.com>
2014-10-01 11:33:24 +0200

    + JavaDoc für IBackupListener

--- src/java/main/IBackupListener.java
+++ src/java/main/IBackupListener.java
@@ -1,9 +1,25 @@
 package main;
 
 public interface IBackupListener {
-	
+
+	/**
+	 * Gibt den übergebenen String auf dem Output-Panel aus und schreibt ihn
+	 * (abhängig vom Level) in die log-Datei.
+	 * 
+	 * @param task
+	 *            zugehöriger Task (zu welchem der Outprint gehört)
+	 * @param s
+	 *            auszugebender String
+	 * @param level
+	 *            Ausgabe Level: 0 = nur ausgeben, 1 = ausgeben und loggen
+	 */
 	public void printOut(BackupTask task, String s, int level);
-	
+
+	/**
+	 * Gibt den aktuell laufenden Task zurück.
+	 * 
+	 * @return aktuell laufender Task
+	 */
 	public BackupTask getCurrentTask();
 
 }
