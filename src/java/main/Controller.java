c5acab9646bb8da8cc1bd9c54beb5b86067c1aa9
andy <andy.fleig@gmail.com>
2014-10-04 14:03:15 +0200

    ! potentielle Endlosschleife auskommentiert

--- src/java/main/Controller.java
+++ src/java/main/Controller.java
@@ -219,7 +219,10 @@ public class Controller {
 		if (level > 0) {
 			// Log-Datei anlegen:
 			if (task == null) {
-				printOut(currentTask, ResourceBundle.getBundle("gui.messages").getString("Messages.ErrorLoggingDisabled"), 0, true);
+				// TODO: Endlos-Schleife:
+				// printOut(currentTask,
+				// ResourceBundle.getBundle("gui.messages").getString("Messages.ErrorLoggingDisabled"),
+				// 0, true);
 				return;
 			}
 			File log = new File(task.getDestinationPath() + System.getProperty("file.separator") + task.getTaskName()
