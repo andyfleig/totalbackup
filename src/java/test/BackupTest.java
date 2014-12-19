bc959f9a65f2ec5be00367cd6b7d8dd7765e6cee
andy <andy.fleig@gmail.com>
2014-12-19 12:42:35 +0100

    + erster JUnit-Test für NormalBackup

--- src/java/test/BackupTest.java
+++ src/java/test/BackupTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import main.BackupTask;
 import main.Controller;
+import main.NormalBackup;
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -9,55 +11,35 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class BackupTest {
-	
-	static File dir;
-	static File dir1;
-	static File dir2;
-	static File dir3;
-	static File testFile1;
-	static File testFile2;
-	static File testFile3;
+
+	static String testFolder = "/home/andy/TotalBackup/test";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		dir = new File("/home/andy/dir");
-		dir.mkdir();
-		dir1 = new File("/home/andy/dir/dir1");
-		dir1.mkdir();
-		dir2 = new File("/home/andy/dir/dir2");
-		dir2.mkdir();
-		dir3 = new File("/home/andy/dir/dir3");
-		dir3.mkdir();
-		
-		testFile1 = new File("/home/andy/dir/dir1/testFile1");
-		//testFile1.mkdirs();
-		testFile1.createNewFile();
-		testFile2 = new File("/home/andy/dir/dir1/testFile2");
-		//testFile2.mkdirs();
-		testFile2.createNewFile();
-		testFile3 = new File("/home/andy/dir/dir2/testFile3");
-		//testFile3.mkdirs();
-		testFile3.createNewFile();
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		/*
-		testFile1.delete();
-		testFile2.delete();
-		testFile3.delete();
-		dir1.delete();
-		dir2.delete();
-		dir3.delete();
-		*/
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		
+
 	}
 
 	@After
@@ -65,14 +47,74 @@ public class BackupTest {
 	}
 
 	@Test
-	public void testRunBackup() {
+	public void testNormalBackup1() {
+		BackupTask task = new BackupTask("test");
+		task.addSourcePath(testFolder + "/source");
+		task.setDestinationPath(testFolder + "/dest");
+
 		Controller c = new Controller();
 		c.startController();
+		c.startBackup(task);
 
-		//c.getMainframe()
-		File f = new File ("/dir");
-		assertEquals(f.list().length, 4);
+		// Kopierte Dateien prüfen:
+		File log = new File(testFolder + "/dest/test.log");
+		log.delete();
+
+		File dest = new File(testFolder + "/dest");
+		String[] filesInDest = dest.list();
+		if (filesInDest.length != 1) {
+			System.out.println("FEHLER!");
+		}
+		File testFile = new File(testFolder + "/dest/" + filesInDest[0] + "/source/testFile");
+
+		File original = new File(testFolder + "/source/testFile");
+		String originalHash;
+		String backupHash;
+		try {
+			originalHash = calcMD5(original);
+		} catch (Exception e) {
+			return;
+		}
+		try {
+			backupHash = calcMD5(testFile);
+		} catch (Exception e) {
+			return;
+		}
 		
-		c = null;
+		System.out.println(originalHash);
+		System.out.println(backupHash);
+		
+		assertTrue(originalHash.equals(backupHash));
+
+		// Backup löschen:
+		testFile.delete();
+		(new File(testFolder + "/dest/" + filesInDest[0] + "/source")).delete();
+		(new File(testFolder + "/dest/" + filesInDest[0])).delete();
 	}
+
+	private String calcMD5(File f) throws Exception {
+		MessageDigest md;
+
+		md = MessageDigest.getInstance("MD5");
+
+		FileInputStream fis;
+
+		fis = new FileInputStream(f.getAbsoluteFile());
+
+		byte[] dataBytes = new byte[1024];
+
+		int nread = 0;
+		while ((nread = fis.read(dataBytes)) != -1) {
+			md.update(dataBytes, 0, nread);
+		}
+		;
+		byte[] mdbytes = md.digest();
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < mdbytes.length; i++) {
+			sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
+	}
+
 }
