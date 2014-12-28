3124a6cff66ac946603708f8653de999e88bf270
andy <andy.fleig@gmail.com>
2014-12-28 20:50:02 +0100

    + JavaDoc im ISourcesListener

--- src/java/gui/ISourcesListener.java
+++ src/java/gui/ISourcesListener.java
@@ -3,8 +3,29 @@ package gui;
 import main.Source;
 
 public interface ISourcesListener {
-	//TODO: JavaDoc
+	/**
+	 * Prüft für den gegebenen String ob dieser bereits Quellpfad ist.
+	 * 
+	 * @param path
+	 *            zu prüfender Quellpfad
+	 * @return ob der Pfad bereits Quellpfad ist
+	 */
 	public boolean isAlreadySourcePath(String path);
+
+	/**
+	 * Fügt eine Quelle hinzu.
+	 * 
+	 * @param source
+	 *            hinzuzufügende Quelle.
+	 */
 	public void addSource(Source source);
+
+	/**
+	 * Sucht nach einer Quelle mit dem gegebenen Pfad. Wird diese gefunden wird
+	 * sie gelöscht.
+	 * 
+	 * @param path
+	 *            zu löschende Quelle
+	 */
 	public void deleteSource(String path);
 }
