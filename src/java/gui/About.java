2270e8b79210d6bc2c40dc0eda91b418bc4e67ff
andy <andy.fleig@gmail.com>
2014-12-29 21:46:46 +0100

    * Über-Dialog

--- src/java/gui/About.java
+++ src/java/gui/About.java
@@ -5,27 +5,32 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.AbstractAction;
+
 import java.awt.event.ActionEvent;
+
 import javax.swing.Action;
+
 import java.awt.event.ActionListener;
+
 import javax.swing.SwingConstants;
+
 import java.awt.Color;
+
 import javax.swing.JTextPane;
+
 import java.util.ResourceBundle;
 
 public class About extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private final Action action = new SA_ok();
-	private JLabel lbl_Copyright;
 	private JButton btn_Ok;
-	private JPanel panel;
-	private JLabel lbl_Version;
-	private JPanel panel_1;
-	private JTextPane txt_About;
+	private JLabel txt_About;
 
 
 	/**
@@ -48,51 +53,29 @@ public class About extends JDialog {
 	 */
 	public About() {
 		setResizable(false);
-		setTitle(ResourceBundle.getBundle("gui.messages").getString("About.this.title")); //$NON-NLS-1$ //$NON-NLS-2$
+		setTitle(ResourceBundle.getBundle("gui.messages").getString("About.this.title"));
 		setModal(true);
 		setAlwaysOnTop(true);
-		setBounds(100, 100, 365, 147);
+		setBounds(100, 100, 480, 240);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(new BorderLayout(0, 0));
 		{
-			panel = new JPanel();
-			contentPanel.add(panel, BorderLayout.SOUTH);
-			panel.setLayout(new BorderLayout(0, 0));
-			{
-				btn_Ok = new JButton(ResourceBundle.getBundle("gui.messages").getString("About.okButton.text")); //$NON-NLS-1$ //$NON-NLS-2$
-				panel.add(btn_Ok, BorderLayout.EAST);
-				btn_Ok.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-					}
-				});
-				btn_Ok.setAction(action);
-				btn_Ok.setActionCommand(ResourceBundle.getBundle("gui.messages").getString("About.okButton.actionCommand")); //$NON-NLS-1$ //$NON-NLS-2$
-				getRootPane().setDefaultButton(btn_Ok);
-			}
-			{
-				panel_1 = new JPanel();
-				panel.add(panel_1, BorderLayout.WEST);
-				panel_1.setLayout(new BorderLayout(0, 0));
-				{
-					lbl_Copyright = new JLabel(ResourceBundle.getBundle("gui.messages").getString("About.label.text")); //$NON-NLS-1$ //$NON-NLS-2$
-					panel_1.add(lbl_Copyright, BorderLayout.SOUTH);
-					lbl_Copyright.setVerticalAlignment(SwingConstants.BOTTOM);
-					lbl_Copyright.setHorizontalAlignment(SwingConstants.CENTER);
-				}
-				{
-					lbl_Version = new JLabel(ResourceBundle.getBundle("gui.messages").getString("About.lblVAlpha.text")); //$NON-NLS-1$ //$NON-NLS-2$
-					panel_1.add(lbl_Version, BorderLayout.NORTH);
-					lbl_Version.setHorizontalAlignment(SwingConstants.LEFT);
-				}
-			}
+			txt_About = new JLabel();
+			txt_About.setText(ResourceBundle.getBundle("gui.messages").getString("About.txtpnWarnungDieSoftware.text"));
+			contentPanel.add(txt_About, BorderLayout.CENTER);
 		}
 		{
-			txt_About = new JTextPane();
-			txt_About.setForeground(Color.RED);
-			txt_About.setText(ResourceBundle.getBundle("gui.messages").getString("About.txtpnWarnungDieSoftware.text")); //$NON-NLS-1$ //$NON-NLS-2$
-			contentPanel.add(txt_About, BorderLayout.CENTER);
+			btn_Ok = new JButton(ResourceBundle.getBundle("gui.messages").getString("About.okButton.text"));
+			contentPanel.add(btn_Ok, BorderLayout.SOUTH);
+			btn_Ok.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+				}
+			});
+			btn_Ok.setAction(action);
+			btn_Ok.setActionCommand(ResourceBundle.getBundle("gui.messages").getString("About.okButton.actionCommand"));
+			getRootPane().setDefaultButton(btn_Ok);
 		}
 	}
 
