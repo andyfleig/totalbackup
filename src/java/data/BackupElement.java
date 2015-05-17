e02e2a98068e415ea7e2a310f69bf623cb16a67c
Andreas Fleig <andy@Galileo.fritz.box>
2015-05-17 19:22:58 +0200

    + Lizenzinformationen

--- src/java/data/BackupElement.java
+++ src/java/data/BackupElement.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2014, 2015 Andreas Fleig (andy DOT fleig AT gmail DOT com)
+ * 
+ * All rights reserved.
+ * 
+ * This file is part of TotalBackup.
+ *
+ * TotalBackup is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TotalBackup is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TotalBackup.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package data;
 
 public class BackupElement {
@@ -17,12 +37,17 @@ public class BackupElement {
 	 * Legt fest ob das Element verlinkt (true) oder kopiert (false) werden soll
 	 */
 	private boolean toLink = false;
-	
+
 	/**
 	 * Erzeugt ein neues Backup-Element.
-	 * @param path Pfad des Elements
-	 * @param isDirectory ob das Element ein Ordner ist
-	 * @param toLink ob das Element kopiert oder verlinkt werden soll (Achtung: Ordner können nicht verlinkt werden)
+	 * 
+	 * @param path
+	 *            Pfad des Elements
+	 * @param isDirectory
+	 *            ob das Element ein Ordner ist
+	 * @param toLink
+	 *            ob das Element kopiert oder verlinkt werden soll (Achtung:
+	 *            Ordner können nicht verlinkt werden)
 	 */
 	public BackupElement(String sourcePath, String destPath, boolean isDirectory, boolean toLink) {
 		this.sourcePath = sourcePath;
@@ -32,19 +57,19 @@ public class BackupElement {
 			this.toLink = toLink;
 		}
 	}
-	
+
 	public String getSourcePath() {
 		return sourcePath;
 	}
-	
+
 	public String getDestPath() {
 		return destPath;
 	}
-	
+
 	public boolean isDirectory() {
 		return isDirectory;
 	}
-	
+
 	public boolean toLink() {
 		return toLink;
 	}
