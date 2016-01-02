82d7bf5a109336a4416982b9466257153ef13fe8
Andreas Fleig <andy.fleig@gmail.com>
2016-01-02 15:41:50 +0100

    * Copyright Infos

--- src/java/listener/INECListener.java
+++ src/java/listener/INECListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014, 2015 Andreas Fleig (andy DOT fleig AT gmail DOT com)
+ * Copyright 2014 - 2016 Andreas Fleig (andy DOT fleig AT gmail DOT com)
  *
  * All rights reserved.
  *
